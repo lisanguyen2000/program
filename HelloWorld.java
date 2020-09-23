343101fae5a48e08a813647f6c3a270c1e514f5d
lisanguyen2000 <70598376+lisanguyen2000@users.noreply.github.com>
2020-09-23 15:47:56 -0700

    added comment

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,13 @@
-/**
- * HelloWorld
+/*
+ * Lisa Nguyen
+ * September 25, 2020
+ * 
+ * This program will print Hello World
  */
 public class HelloWorld {
 
     public static void main(String[] args) {
+        //this will print Hello World to output
         System.out.println("Hello World");
     }
 }
